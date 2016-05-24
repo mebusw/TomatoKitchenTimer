22042f584f3854e6da1569f86b7663fd18bc7e2d
mebusw <mebusw@163.com>
2016-05-24 10:31:07 +0800

    init the Timers with NSTimer and NSDateFormatter

--- BigTimer/ViewController.swift
+++ BigTimer/ViewController.swift
@@ -10,16 +10,29 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet var timeText:NSTextFieldCell?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: #selector(ViewController.tick(_:)), userInfo: nil, repeats: true)
+        
+        
     }
 
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    func tick(timer : NSTimer) {
+        let timestamp = NSDateFormatter.localizedStringFromDate(NSDate(), dateStyle: .ShortStyle, timeStyle: .LongStyle)
+        print(timestamp)
+        print(timer.userInfo)
+        timeText?.title = timestamp
     }
 
 

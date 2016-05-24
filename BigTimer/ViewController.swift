4dddcc01e04001417cb310087f179dbf0a505a0f
mebusw <mebusw@163.com>
2016-05-24 10:10:51 +0800

    Initial Commit

---
+++ BigTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BigTimer
+//
+//  Created by JackyShen on 16/5/24.
+//  Copyright © 2016年 JackyShen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
